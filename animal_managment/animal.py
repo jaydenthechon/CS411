89b142abbd73aa7e029655967c652a0e73260dcd
jaydenthechon <144143056+jaydenthechon@users.noreply.github.com>
2024-10-07 23:48:08 -0400

    Add files via upload

---
+++ animal_managment/animal.py
@@ -0,0 +1,11 @@
+from typing import Any, Optional
+
+class Animal:
+
+    def __init__(self, animal: Optional[Any] = None):
+        self.animal = animal
+
+    def get_animal_details(animal_id) -> dict[str, Any]:
+        pass
+    def update_animal_details(animal_id: int, **kwargs: Any) -> None:
+        pass
